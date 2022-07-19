3d01472dc38104ad7894009710cbf373bbc8953a
Asier Cidon <asier.cidon@gmail.com>
2022-07-19 15:18:58 +0200

    Added error control

--- pkg/performance/ns_creation.go
+++ pkg/performance/ns_creation.go
@@ -1,6 +1,7 @@
 package performance
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 	"testing"
@@ -18,51 +19,71 @@ func arrayPositionFind(a []string, x string) int {
 	return -1
 }
 
-func deleteNSBundle(min int, max int) {
+func deleteNSBundle(min int, max int) error {
 	util.Log.Info("Deleting namespaces from ", min, " to ", max)
 	for i := min; i < max; i++ {
 		nsName := appNSPrefix + strconv.Itoa(i)
-		delNamespaceMesh(nsName)
+		err := delNamespaceMesh(nsName)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
-func createNSBundle(min int, max int) {
+func createNSBundle(min int, max int) error {
 	util.Log.Info("Creating namespaces from ", min, " to ", max)
 	for i := min; i < max; i++ {
 		nsName := appNSPrefix + strconv.Itoa(i)
-		createNamespaceMesh(nsName)
+		err := createNamespaceMesh(nsName)
+		if err != nil {
+			return err
+		}
 	}
+	return nil
 }
 
-func testNamespaceAdditionTime(index int, acceptanceTime int, t *testing.T) {
+func testNamespaceAdditionTime(index int, acceptanceTime int) error {
 	// Convert second to milisecond
 	acceptanceTimeMS := acceptanceTime * 1000
 	util.Log.Info("Adding a new namespace to the mesh (Acceptance Time: ", acceptanceTimeMS, " miliseconds)")
 
 	// Create namespace
 	nsName := appNSPrefix + strconv.Itoa(index) + "-measure"
-	createNS(nsName)
+	err := createNS(nsName)
+	if err != nil {
+		return err
+	}
 
 	// Measure namespace addition to the mesh time
 	startT := time.Now()
-	addNamespaceMesh(nsName)
+	errAddNS := addNamespaceMesh(nsName)
+	if errAddNS != nil {
+		return err
+	}
 	duration := int(time.Since(startT) / (time.Second / time.Microsecond))
 
 	// Check addition time
 	if duration > acceptanceTimeMS {
 		util.Log.Error("Acceptance time exceeded")
-		t.Error("Acceptance time exceeded")
-		t.FailNow()
+		return fmt.Errorf("Acceptance time exceeded")
 	}
 	util.Log.Info("Duration OK: ", duration)
+	return nil
 }
 
 func TestNSAdditionTime(t *testing.T) {
 	util.Log.Info("** TEST: TestNSAdditionTime")
 
+	// Test SMMR and SMCP are Ready
+	TestSMCP(t)
+	TestSMMR(t)
+
 	// Iterate namespace bundles
 	nsCounts := strings.Split(nsCountBundle, ",")
+	var step int
 	for i, s := range nsCounts {
+		step = i
 		// Define required variables
 		count, _ := strconv.Atoi(s)
 		acceptanceTime, _ := strconv.Atoi(nsAcceptanceTime)
@@ -70,22 +91,37 @@ func TestNSAdditionTime(t *testing.T) {
 		// Create required namespaces taking into account previous namespaces creation
 		if i > 0 {
 			tmp, _ := strconv.Atoi(nsCounts[i-1])
-			createNSBundle(tmp, count)
+			err := createNSBundle(tmp, count)
+			if err != nil {
+				t.Error(err.Error())
+				break
+			}
 		} else {
-			createNSBundle(0, count)
+			err := createNSBundle(0, count)
+			if err != nil {
+				t.Error(err.Error())
+				break
+			}
 		}
 
 		// Wait for SMMR is Ready
 		TestSMMR(t)
 
 		// Measure a new namespace creation
-		testNamespaceAdditionTime(count, acceptanceTime, t)
+		err := testNamespaceAdditionTime(count, acceptanceTime)
+		if err != nil {
+			t.Error(err.Error())
+			break
+		}
 	}
 
 	// clean namespaces
 	util.Log.Info("cleaning up test")
-	max, _ := strconv.Atoi(nsCounts[len(nsCounts)-1])
-	deleteNSBundle(0, max)
+	max, _ := strconv.Atoi(nsCounts[step])
+	err := deleteNSBundle(0, max)
+	if err != nil {
+		t.Error(err.Error())
+	}
 	for _, s := range nsCounts {
 		nsName := appNSPrefix + s + "-measure"
 		delNamespaceMesh(nsName)
--- pkg/performance/utils.go
+++ pkg/performance/utils.go
@@ -1,6 +1,7 @@
 package performance
 
 import (
+	"net/http"
 	"strconv"
 	"strings"
 	"testing"
@@ -10,7 +11,7 @@ import (
 
 func TestSMCP(t *testing.T) {
 	util.Log.Info("Checking SMCP in ", meshNamespace)
-	msg, _ := util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 300s`, meshNamespace, smcpName)
+	msg, _ := util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 30s`, meshNamespace, smcpName)
 	if !strings.Contains(msg, "condition met") {
 		util.Log.Error("SMCP not Ready")
 		t.Error("SMCP not Ready")
@@ -20,7 +21,7 @@ func TestSMCP(t *testing.T) {
 
 func TestSMMR(t *testing.T) {
 	util.Log.Info("Checking SMMR in ", meshNamespace)
-	msg, _ := util.Shell(`oc wait --for condition=Ready -n %s smmr/default --timeout 300s`, meshNamespace)
+	msg, _ := util.Shell(`oc wait --for condition=Ready -n %s smmr/default --timeout 30s`, meshNamespace)
 	if !strings.Contains(msg, "condition met") {
 		util.Log.Error("SMMR not Ready")
 		t.Error("SMMR not Ready")
@@ -28,17 +29,27 @@ func TestSMMR(t *testing.T) {
 	}
 }
 
-func deleteNS(namespace string) {
+func deleteNS(namespace string) error {
 	util.Log.Debug("Deleting namespace", namespace)
-	util.ShellSilent(`oc delete project %s`, namespace)
+	_, err := util.ShellSilent(`oc delete project %s`, namespace)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func createNS(namespace string) {
+func createNS(namespace string) error {
 	util.Log.Debug("Creating namespace", namespace)
-	util.ShellSilent(`oc new-project %s`, namespace)
+	_, err := util.ShellSilent(`oc new-project %s`, namespace)
+	if err != nil {
+		if !strings.Contains(err.Error(), "AlreadyExists") {
+			return err
+		}
+	}
+	return nil
 }
 
-func delNamespaceMesh(namespace string) {
+func delNamespaceMesh(namespace string) error {
 	// Find namespace in members array
 	tmp, _ := util.ShellSilent(`oc get smmr default -n %s --template='{{ .spec.members }}'`, meshNamespace)
 	members := strings.Split(tmp, " ")
@@ -46,43 +57,101 @@ func delNamespaceMesh(namespace string) {
 
 	// If namespace exists
 	if position > 0 {
-		util.ShellSilent(`oc patch smmr default -n %s --type='json' -p='[{"op": "remove", "path": "/spec/members/%s"}]'`, meshNamespace, strconv.Itoa(position))
+		// Path SMMR
+		_, err := util.ShellSilent(`oc patch smmr default -n %s --type='json' -p='[{"op": "remove", "path": "/spec/members/%s"}]'`, meshNamespace, strconv.Itoa(position))
+		if err != nil {
+			return err
+		}
+
+		// Verify SMMR deleting a NS
 		configured := false
 		util.Log.Debug("Waiting for the namespace to be deleted to the mesh: ", namespace)
 		for !configured {
-			msgConfigured, _ := util.ShellSilent(`oc get smmr default -n %s --template='{{ .status.configuredMembers }}'`, meshNamespace)
-			msgPending, _ := util.ShellSilent(`oc get smmr default -n %s --template='{{ .status.pendingMembers }}'`, meshNamespace)
+			msgConfigured, errConfigured := util.ShellSilent(`oc get smmr default -n %s --template='{{ .status.configuredMembers }}'`, meshNamespace)
+			if errConfigured != nil {
+				return errConfigured
+			}
+			msgPending, errPending := util.ShellSilent(`oc get smmr default -n %s --template='{{ .status.pendingMembers }}'`, meshNamespace)
+			if errPending != nil {
+				return errConfigured
+			}
 			if !strings.Contains(msgConfigured, namespace+" ") && !strings.Contains(msgPending, namespace+"]") {
 				if !strings.Contains(msgPending, namespace+" ") && !strings.Contains(msgPending, namespace+"]") {
-					deleteNS(namespace)
+					err := deleteNS(namespace)
+					if err != nil {
+						return err
+					}
 					configured = true
 				}
 			}
 		}
 	}
+	return nil
 }
 
-func createNamespaceMesh(namespace string) {
-	createNS(namespace)
-	util.ShellSilent(`oc patch smmr default -n %s --type='json' -p='[{"op": "add", "path": "/spec/members/-", "value":"%s"}]'`, meshNamespace, namespace)
+func createNamespaceMesh(namespace string) error {
+	// Create NS
+	err := createNS(namespace)
+	if err != nil {
+		return err
+	}
+
+	// Path SMMR
+	_, errPatch := util.ShellSilent(`oc patch smmr default -n %s --type='json' -p='[{"op": "add", "path": "/spec/members/-", "value":"%s"}]'`, meshNamespace, namespace)
+	if errPatch != nil {
+		return err
+	}
+
+	// Verify SMMR after adding a new NS
 	configured := false
 	util.Log.Debug("Waiting for the namespace to be added to the mesh: ", namespace)
 	for !configured {
-		msg, _ := util.ShellSilent(`oc get smmr default -n %s --template='{{ .status.configuredMembers }}'`, meshNamespace)
+		msg, err := util.ShellSilent(`oc get smmr default -n %s --template='{{ .status.configuredMembers }}'`, meshNamespace)
+		if errPatch != nil {
+			return err
+		}
 		if strings.Contains(msg, namespace) {
 			configured = true
+			return nil
 		}
 	}
+	return nil
 }
 
-func addNamespaceMesh(namespace string) {
-	util.ShellSilent(`oc patch smmr default -n %s --type='json' -p='[{"op": "add", "path": "/spec/members/-", "value":"%s"}]'`, meshNamespace, namespace)
+func addNamespaceMesh(namespace string) error {
+	// Path SMMR
+	_, err := util.ShellSilent(`oc patch smmr default -n %s --type='json' -p='[{"op": "add", "path": "/spec/members/-", "value":"%s"}]'`, meshNamespace, namespace)
+	if err != nil {
+		return err
+	}
+
+	// Verify SMMR after adding a new NS
 	configured := false
 	util.Log.Debug("Waiting for the namespace to be added to the mesh: ", namespace)
 	for !configured {
-		msg, _ := util.ShellSilent(`oc get smmr default -n %s --template='{{ .status.configuredMembers }}'`, meshNamespace)
+		msg, err := util.ShellSilent(`oc get smmr default -n %s --template='{{ .status.configuredMembers }}'`, meshNamespace)
+		if err != nil {
+			return err
+		}
 		if strings.Contains(msg, namespace) {
 			configured = true
+			return nil
 		}
 	}
+	return nil
+}
+
+func GetWithJWT(url, token, host string) (*http.Response, error) {
+	// Declare http client
+	client := &http.Client{}
+
+	// Declare HTTP Method and Url
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Host = host
+	req.Header.Set("Host", req.Host)
+	req.Header.Add("Authorization", "Bearer "+token)
+	return client.Do(req)
 }
